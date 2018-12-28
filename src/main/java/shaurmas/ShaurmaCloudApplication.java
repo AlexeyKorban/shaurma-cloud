e60d7ecc9810b77eefeaf9d1d53158feca6853c0
Корбан Алексей <ldwx@mail.ru>
2018-12-28 11:31:31 +0500

    Fix names

--- src/main/java/shaurmas/ShaurmaCloudApplication.java
+++ src/main/java/shaurmas/ShaurmaCloudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShaurmaCoudApplication {
+public class ShaurmaCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShaurmaCoudApplication.class, args);
+		SpringApplication.run(ShaurmaCloudApplication.class, args);
 	}
 
 }
--- src/test/java/shaurmas/ShaurmaCloudApplicationTests.java
+++ src/test/java/shaurmas/ShaurmaCloudApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShaurmaCoudApplicationTests {
+public class ShaurmaCloudApplicationTests {
 
 	@Test
 	public void contextLoads() {
