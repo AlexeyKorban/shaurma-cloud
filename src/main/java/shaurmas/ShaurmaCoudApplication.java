d953ad2dabd19c99c54aa787dc7979c60e87c5da
Корбан Алексей <alexey.korban@dil-it.ru>
2018-12-28 11:16:52 +0500

    Initial commit

---
+++ src/main/java/shaurmas/ShaurmaCoudApplication.java
@@ -0,0 +1,14 @@
+package shaurmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaurmaCoudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShaurmaCoudApplication.class, args);
+	}
+
+}
+
