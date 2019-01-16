26075c34cf5504c21ec629f264d253af5c3b578e
Корбан Алексей <ldwx@mail.ru>
2019-01-16 18:02:03 +0500

    Add validation

--- src/main/java/shaurmas/web/DesignShaurmaController.java
+++ src/main/java/shaurmas/web/DesignShaurmaController.java
@@ -3,13 +3,16 @@ package shaurmas.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import shaurmas.Ingredient;
 import shaurmas.Ingredient.Type;
 import shaurmas.Shaurma;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,7 +48,12 @@ public class DesignShaurmaController {
     }
 
     @PostMapping
-    public String processDesign() {
+    public String processDesign(@Valid
+                                    @ModelAttribute("design")
+                                            Shaurma design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         log.info("Processing design: " + "design");
         return "redirect:/orders/current";
     }
