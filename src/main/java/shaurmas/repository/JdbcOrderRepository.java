5ae50fdafebbf61dabc6418a49857358bc678aa4
AlexeyKorban <ldwx@mail.ru>
2019-02-12 20:41:03 +0500

    Add order to order controller

--- src/main/java/shaurmas/repository/JdbcOrderRepository.java
+++ src/main/java/shaurmas/repository/JdbcOrderRepository.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
 import shaurmas.Order;
 import shaurmas.Shaurma;
 
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public class JdbcOrderRepository implements OrderRepository {
 
     private SimpleJdbcInsert orderInserter;
