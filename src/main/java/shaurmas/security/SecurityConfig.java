6eadf062b2340d792ea53b77811dcaae0ddb7082
AlexeyKorban <ldwx@mail.ru>
2019-02-18 19:38:41 +0500

    Add in memory user storage

---
+++ src/main/java/shaurmas/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package shaurmas.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("user")
+                .password("password")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("user2")
+                .password("strong_password")
+                .authorities("ROLE_USER");
+    }
+}
