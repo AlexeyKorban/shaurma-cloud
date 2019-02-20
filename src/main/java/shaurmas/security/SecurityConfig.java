3efcae2ea68813f44f9eacb5a4009f802f7147d9
AlexeyKorban <ldwx@mail.ru>
2019-02-20 20:19:28 +0500

    Add logback properties

--- src/main/java/shaurmas/security/SecurityConfig.java
+++ src/main/java/shaurmas/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package shaurmas.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -22,18 +21,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user2")
                 .password("strong_password")
                 .authorities("ROLE_USER");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/design", "/orders")
-                .hasRole("ROLE_USER")
-                .antMatchers("/", "/**").permitAll()
-
-                .and()
-                .formLogin()
-                .loginPage("/login");
     }
 }
--- src/main/java/shaurmas/web/HomeController.java
+++ src/main/java/shaurmas/web/HomeController.java
@@ -2,7 +2,6 @@ package shaurmas.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Controller
 public class HomeController {
